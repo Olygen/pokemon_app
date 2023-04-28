2a529aa489a4349a160a654c67974a66ba93c43e
Oligen <olga.sarukhanova@gmail.com>
2023-04-28 17:49:08 -0400

    "index.jsx view rendered at pokemon route"

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const pokemon = require('./models/pokemon');
 
+// Middleware
+const reactViewsEngine = require("jsx-view-engine").createEngine();
+app.engine('jsx', reactViewsEngine);
+// This line tells the render method the default file extension to look for
+app.set("view engine", "jsx");
+// This line sets the render method's default location to look for a jsx file to render. Without this line of code we would have specific the views directory every time we use the render method
+app.set("Views", "./views");
+
+// Custom middleware
+app.use(express.urlencoded({ extended:false} ));
+
 //I.N.D.U.C.E.S. for pokemons
 //=========================
 // Index
@@ -11,10 +22,10 @@ app.get('/', (req, res) => {
     res.send(`<h1> Welcome to the Pokemon App!</h1>`);
   });
 
-app.get('/pokemon', (req, res) => {
+  app.get('/pokemon', (req, res) => {
     console.log('Index Controller Func. running...');
-    res.send(pokemon);
-})
+    res.render("Index", {pokemon});
+});
 
 //Listen
 app.listen(PORT, () => {
