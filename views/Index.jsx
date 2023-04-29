c30734bb89cbbe8d5a80b64448e9d132e502d391
Oligen <olga.sarukhanova@gmail.com>
2023-04-29 18:47:09 -0400

    "Created show views of each pokemon"

--- views/Index.jsx
+++ views/Index.jsx
@@ -3,7 +3,24 @@ const React = require("react");
 const header = {
   color: '#ffffff',
   backgroundColor: '#000000',
+  textAlign: 'center'
 };
+
+const mainPage = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+
+//   const mainPage = {
+//     display: 'grid',
+//     gridTemplateColumns: '15vw 15vw 15vw',
+//     gridTemplateRows: '15vw 15vw 15vw',
+//     justifyItems: 'stretch',
+//     justifyContent: 'center',
+//   }
+
 
 class Index extends React.Component {
   render() {
@@ -13,26 +30,24 @@ class Index extends React.Component {
         <header style={header}>
             <h1>See All The Pokemon!</h1>         
         </header>
-        <ul>
+        <container  style={mainPage}>
             {pokemon.map((pokemon, i) => {
                 // Capitalize first letter of pokemon name
-                const capitalizedPokemonName =
-              pokemon.name.slice(0, 1).toUpperCase() + pokemon.name.slice(1);
+            //     const capitalizedPokemonName =
+            //   pokemon.name.slice(0, 1).toUpperCase() + pokemon.name.slice(1);
               
                 // Add .jpg to the image link
                 const pokemonImageLink = pokemon.img + ".jpg";
 
                 return (
-                    <li key={i}>
-                        {/* The name is  {" "} */}
+                    <div key={i}>
                         <a href={`/pokemon/${i}`}>
-                            {capitalizedPokemonName}
+                        <img style={{ width: '40%' }} src={pokemonImageLink} alt={`${pokemon.name} image`} />
                         </a>{" "}
-                        <img src={pokemonImageLink} alt={`${pokemon.name} image`} />
-                    </li>                     
+                    </div>                     
                 );
             })}
-        </ul>
+        </container>
       </div>
     );
   }
--- views/Show.jsx
+++ views/Show.jsx
@@ -1,5 +1,11 @@
 const React = require('react');
 
+const div = {
+  textAlign: 'center',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+
 const header = {
   color: '#ffffff',
   backgroundColor: '#800080',
@@ -8,12 +14,15 @@ const header = {
 class Show extends React.Component {
    render () {
     const pokemon = this.props.pokemon;
+    // Add .jpg to the image link
+    const pokemonImageLink = pokemon.img + ".jpg";
+
     return (
-        <div>
+        <div style={div}>
             <header style={header}>
                 <h1>Gotta Catch {pokemon.name.slice(0, 1).toUpperCase() + pokemon.name.slice(1)}!</h1>         
             </header>
-            {pokemon.img}
+            <img src={pokemonImageLink} alt={`${pokemon.name} image`} />
             <a href={`/pokemon`}>
                 <h2>Back to all pokemon</h2>
             </a>
